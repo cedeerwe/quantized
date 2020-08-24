e2a8f541832d9c36d6a05bbc9ec707816601ebf4
cd <cdominik@gmail.com>
2020-08-24 08:31:24 +0200

    follow_path

--- td/src/main.rs
+++ td/src/main.rs
@@ -1,6 +1,4 @@
 use bevy::prelude::*;
-use rand::Rng;
-use std::f32::consts::PI;
 
 fn setup(mut commands: Commands) {
     commands
@@ -8,82 +6,142 @@ fn setup(mut commands: Commands) {
         .spawn(UiCameraComponents::default());
 }
 
-struct SpawnTimer(Timer);
+#[derive(Debug, Clone)]
+struct FollowPath {
+    path: Vec<Vec2>,
+    current_origin: usize,
+    total_part: f32,
+    current_part: f32,
+    cumulative_relative_lengths: Vec<f32>,
+}
 
-struct Ball {
-    pub velocity: Vec3,
-    pub sine_bias_term: f32,
+impl FollowPath {
+    fn new(path: Vec<Vec2>) -> Self {
+        let mut lengths = vec![];
+        let mut cumulative_lengths = vec![0.];
+        for (i, point) in path.iter().enumerate() {
+            if i == 0 {
+                continue;
+            }
+            let length = (*point - path[i - 1]).length();
+            lengths.push(length);
+            cumulative_lengths.push(length + cumulative_lengths[i - 1]);
+        }
+        let total_length = cumulative_lengths.last().unwrap();
+        Self {
+            path,
+            current_origin: 0,
+            total_part: 0.,
+            current_part: 0.,
+            cumulative_relative_lengths: cumulative_lengths
+                .iter()
+                .map(|v| v / total_length)
+                .collect(),
+        }
+    }
+
+    fn position(&self) -> Vec2 {
+        if self.total_part <= 0.0 {
+            *self.path.first().unwrap()
+        } else if self.total_part >= 1.0 {
+            *self.path.last().unwrap()
+        } else {
+            self.path[self.current_origin]
+                .lerp(self.path[self.current_origin + 1], self.current_part)
+        }
+    }
+
+    fn move_part(&mut self, s: f32) {
+        if self.path.len() < 2 {
+            return;
+        }
+        self.total_part = (s + self.total_part).min(1.).max(0.);
+        let mut current_origin = 0;
+        for (i, val) in self.cumulative_relative_lengths.iter().enumerate().rev() {
+            if *val <= self.total_part {
+                current_origin = i;
+                break;
+            }
+        }
+        self.current_origin = current_origin;
+        if self.current_origin == self.path.len() - 1 {
+            return;
+        }
+        self.current_part = (self.total_part
+            - self.cumulative_relative_lengths[self.current_origin])
+            / (self.cumulative_relative_lengths[self.current_origin + 1]
+                - self.cumulative_relative_lengths[self.current_origin]);
+    }
+}
+
+struct EnemySpawner {
+    follow_path: FollowPath,
+    timer: Timer,
 }
 
 fn spawn_dots(
     mut commands: Commands,
     mut materials: ResMut<Assets<ColorMaterial>>,
     time: Res<Time>,
-    mut timer: ResMut<SpawnTimer>,
-    window: Res<WindowDescriptor>,
+    mut spawner: ResMut<EnemySpawner>,
 ) {
-    timer.0.tick(time.delta_seconds);
+    spawner.timer.tick(time.delta_seconds);
 
-    if timer.0.finished {
+    if spawner.timer.finished {
         commands
             .spawn(SpriteComponents {
-                material: materials.add(Color::rgb(1., 1., 1.).into()),
-                translation: Translation(Vec3::new(
-                    rand::thread_rng()
-                        .gen_range(-(window.width as f32) * 0.5, (window.width as f32) * 0.5)
-                        as f32,
-                    (window.height as f32) * 0.5,
-                    1.0,
-                )),
+                material: materials.add(Color::rgb(1., 0.1, 0.1).into()),
+                translation: Translation(spawner.follow_path.position().extend(1.0)),
                 sprite: Sprite {
                     size: Vec2::new(3.0, 3.0),
                 },
                 ..Default::default()
             })
-            .with(Ball {
-                velocity: 60.0 * Vec3::new(0.0, -1.0, 0.0).normalize(),
-                sine_bias_term: rand::thread_rng().gen_range(0., PI),
-            });
-        timer.0.reset();
+            .with(spawner.follow_path.clone());
+        spawner.timer.reset();
     }
 }
 
-fn move_dots(time: Res<Time>, mut ball_query: Query<(&mut Ball, &mut Translation)>) {
+fn move_dots(time: Res<Time>, mut follow_path_query: Query<(&mut FollowPath, &mut Translation)>) {
     // clamp the timestep to stop the ball from escaping when the game starts
     let delta_seconds = f32::min(0.2, time.delta_seconds);
 
-    for (mut ball, mut translation) in &mut ball_query.iter() {
-        translation.0 += ball.velocity * delta_seconds;
-        let bias = ball.sine_bias_term;
-        ball.velocity += Vec3::new(
-            0.2 * (time.seconds_since_startup as f32 + bias).cos(),
-            0.,
-            0.,
-        )
+    for (mut follow_path, mut translation) in &mut follow_path_query.iter() {
+        follow_path.move_part(delta_seconds / 30.);
+        translation.0 = follow_path.position().extend(1.0);
     }
 }
 
-fn remove_dots(
-    mut commands: Commands,
-    window: Res<WindowDescriptor>,
-    mut ball_query: Query<(Entity, &Translation, &Ball)>,
-) {
-    for (entity, translation, _) in &mut ball_query.iter() {
-        if translation.0.y() < -(window.height as f32 * 0.5) {
+fn remove_dots(mut commands: Commands, mut ball_query: Query<(Entity, &FollowPath)>) {
+    for (entity, follow_path) in &mut ball_query.iter() {
+        if follow_path.total_part >= 1. {
             commands.despawn(entity);
         }
     }
 }
 
 fn main() {
+    let path = vec![
+        Vec2::new(-200., -200.),
+        Vec2::new(-200., -100.),
+        Vec2::new(200., -100.),
+        Vec2::new(200., 100.),
+        Vec2::new(0., 200.),
+        Vec2::new(-200., 100.),
+        Vec2::new(0., 0.),
+    ];
+    let follow_path = FollowPath::new(path);
     App::build()
-        .add_resource(SpawnTimer(Timer::from_seconds(0.1)))
         .add_resource(WindowDescriptor {
             title: "TD".to_string(),
             width: 1000,
             height: 600,
             vsync: true,
             ..Default::default()
+        })
+        .add_resource(EnemySpawner {
+            follow_path,
+            timer: Timer::from_seconds(0.5),
         })
         .add_default_plugins()
         .add_startup_system(setup.system())
